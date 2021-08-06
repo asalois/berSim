333b880d0d046e6535c09000da55cad5545abc19
Alex Salois <asalois.scholar@gmail.com>
2021-08-06 14:00:31 -0600

    from shared webex meeting

--- simulation.m
+++ simulation.m
@@ -4,8 +4,8 @@
 clear; clc; close all; % clean up
 rng(123) % set for repeatabilty
 tic % start timing
-nSyms = 2^15; % number of symbols to sim
-nSamples = 2^7; % number of samples per symbol
+nSyms = 2^14; % number of symbols to sim
+nSamples = 2^5; % number of samples per symbol
 M = 4; % modulation order
 msg = randi([0 M-1],nSyms,1); % the msg to send
 symbols = pammod(msg,M); % the symbols to send
@@ -17,8 +17,9 @@ pulse = gaussmf(x ,[nSamples/4 nSamples/2]); % gaussian pluse
 pulse = pulse / sum(pulse); % normalize
 pulseShapedSig = filter(pulse,1,sig); % filter the signal for pulse shape
 
-% figure()
-% plot(x,pulse) % plot pulse 
+figure()
+plot(x,pulse) % plot pulse 
+xlabel('samples')
 
 %% remove delay
 delay = nSamples/2;
@@ -26,23 +27,35 @@ pulseShapedSig = pulseShapedSig(delay+1:end); % cut from tip
 sig = sig(1:end-delay); % cut from tail
 
 %% add Noise
-niosySig = awgn(pulseShapedSig,10,'measured');
+niosySig = awgn(pulseShapedSig,25,'measured');
 
 %% plot the signals
-% figure()
-% hold on
-% plot(sig)
-% % plot(pulseShapedSig)
-% plot(niosySig)
-% hold off
-% toc
+figure()
+hold on
+plot(sig)
+% plot(pulseShapedSig)
+plot(niosySig)
+hold off
+toc
+
 %% plot eye diagrams
-% eyediagram(sig,nSamples)
-% eyediagram(pulseShapedSig,nSamples)
-% eyediagram(niosySig,nSamples)
+% per = nSamples;
+% offset = nSamples/2;
+% eyediagram(sig,nSamples,per,offset)
+% eyediagram(pulseShapedSig,nSamples,per,offset)
+% eyediagram(niosySig,nSamples,per,offset)
 % toc
+% 
+% per = nSamples;
+% offset = 0;
+% eyediagram(sig,nSamples,per,offset)
+% eyediagram(pulseShapedSig,nSamples,per,offset)
+% eyediagram(niosySig,nSamples,per,offset)
+% toc
+
 %% pick samples for BER
 start = delay; % the delay
+% start = 1; % the delay
 picks = niosySig(start:nSamples:end); % simulated
 correctPicks = sig(start:nSamples:end); % refrence
 
@@ -79,11 +92,10 @@ figure()
 semilogy(x,berR','-*')
 xlabel('SNR [dB]')
 ylabel('BER')
-% legend('NO EQ','LMS','DFE 128','DFE 256','Location','southwest')
+%legend('NO EQ','LMS','DFE 128','DFE 256','Location','southwest')
 titleName = sprintf('PAM %d with %0.4g Symbols',M,nSyms);
 title(titleName)
 saveName = sprintf('pam_%03d_simulation.png',M);
 saveas(gcf,saveName)
-% save('Eqsfor13mPOF')
 
 toc
